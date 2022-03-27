eb290aa8b4ed8af343c2a634855a5a3683da51e5
Harry <hareshwarp649>
2022-03-27 17:25:12 +0530

    AddressBook With Person Contact as Csv File

---
+++ TddAddBookJson15/CsvJsonHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace TddAddBookJson15
+{
+    public class CsvJsonHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importPath =@"";
+            string exportpath = @"C:\Users\91936\source\repos\ThirdPartyAccess\ThirdPartyAccess\ExportCSV.csv";
+            StreamReader reader = new StreamReader(importPath);
+            //using var reader = new StreamReader(importPath);
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressBook>().ToList();
+                Console.WriteLine(" Read data successfully from address.csv");
+                foreach (AddressBook addressData in records)
+                {
+                    Console.WriteLine(" " + addressData.firstname);
+                    Console.WriteLine(" " + addressData.lastname);
+                    Console.WriteLine(" " + addressData.address);
+                    Console.WriteLine(" " + addressData.state);
+                    Console.WriteLine(" " + addressData.city);
+                    Console.WriteLine(" " + addressData.code);
+
+
+
+                }
+                Console.WriteLine(("\n Now reading from csv file and write to csv file"));
+                using (var writer = new StreamWriter(exportpath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+
+            }
+        }
+    }
+}
---
+++ TddAddBookJson15/JsonHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+using System.Globalization;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddAddBookJson15
+{
+    public class JsonHandler
+    {
+        public static void ImplementJSON()
+        {
+            string importPath = @"C:\Users\ADMIN\source\repos\ThirdPartyLybrary\TddAddBookJson15\Export.csv";
+            string exportpath = @"C:\Users\ADMIN\source\repos\ThirdPartyLybrary\TddAddBookJson15\JsonTdd";
+            StreamReader reader = new StreamReader(importPath);
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressBook>().ToList();
+                Console.WriteLine(" Read data successfully from address.csv");
+                foreach (AddressBook addressData in records)
+                {
+                    Console.WriteLine(" " + addressData.firstname);
+                    Console.WriteLine(" " + addressData.lastname);
+                    Console.WriteLine(" " + addressData.address);
+                    Console.WriteLine(" " + addressData.state);
+                    Console.WriteLine(" " + addressData.city);
+                    Console.WriteLine(" " + addressData.code);
+
+
+
+                }
+                Console.WriteLine(("\n Now reading from csv file and write to Json file"));
+
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportpath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
+
+  
+    
+
